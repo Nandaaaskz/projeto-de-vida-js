8773f71849d057167c44d7c45d18b2fd085609e5
Nanda <107196595+Nandaaaskz@users.noreply.github.com>
2024-04-19 10:43:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
 
-for (let i=0; i<botoes.length; i++) {
-    botoes[i].onclick = function() {
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for (let j=0; j<botoes.length; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo")
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo")
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempo0objetivo1 = new Date ('2024-07-08T00:00:00');
+
+
+
+
+contadores[0].textContent = calculaTempo (tempo0objetivo1)
+
+function calculaTempo (tempo0objetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempo0objetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60; 
+    horas %= 24;
+    
 }
