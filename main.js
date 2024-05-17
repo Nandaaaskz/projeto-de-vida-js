0e8d1b25baa82723946fc764df64bf3d4d86f019
Nanda <107196595+Nandaaaskz@users.noreply.github.com>
2024-05-17 10:38:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ('2024-11-03T00:00:00');
-const tempoObjetivo2 = new Date ('2024-11-03T00:00:00');
-const tempoObjetivo3 = new Date ('2024-11-03T00:00:00');
-const tempoObjetivo4 = new Date ('2024-11-03T00:00:00');
+const tempoObjetivo2 = new Date ('2028-01-01T00:00:00');
+const tempoObjetivo3 = new Date ('2024-11-10T00:00:00');
+const tempoObjetivo4 = new Date ('2025-01-13T00:00:00');
 
 const tempos = [tempoObjetivo1,tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
